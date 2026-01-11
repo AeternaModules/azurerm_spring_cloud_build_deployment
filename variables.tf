9ea3afdaa5f398ad23e3754a58cddef060ec46d1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:23:57 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "spring_cloud_build_deployments" {
+  description = <<EOT
+Map of spring_cloud_build_deployments, attributes below
+Required:
+    - build_result_id
+    - name
+    - spring_cloud_app_id
+Optional:
+    - addon_json
+    - application_performance_monitoring_ids
+    - environment_variables
+    - instance_count
+    - quota (block):
+        - cpu (optional)
+        - memory (optional)
+EOT
+
+  type = map(object({
+    build_result_id                        = string
+    name                                   = string
+    spring_cloud_app_id                    = string
+    addon_json                             = optional(string)
+    application_performance_monitoring_ids = optional(list(string))
+    environment_variables                  = optional(map(string))
+    instance_count                         = optional(number, 1)
+    quota = optional(object({
+      cpu    = optional(string)
+      memory = optional(string)
+    }))
+  }))
+}
+
