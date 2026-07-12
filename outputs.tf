c8a002e6ac9b299fe43f38cfe138817ca3a6b653
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:31:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "spring_cloud_build_deployments_id" {
+  description = "Map of id values across all spring_cloud_build_deployments, keyed the same as var.spring_cloud_build_deployments"
+  value       = { for k, v in azurerm_spring_cloud_build_deployment.spring_cloud_build_deployments : k => v.id }
+}
 output "spring_cloud_build_deployments_addon_json" {
   description = "Map of addon_json values across all spring_cloud_build_deployments, keyed the same as var.spring_cloud_build_deployments"
   value       = { for k, v in azurerm_spring_cloud_build_deployment.spring_cloud_build_deployments : k => v.addon_json }
